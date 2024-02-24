6f19f8d23f4c3e87b5e568722af0eb841899942a
Abelaash <abelaash.giritharan@gmail.com>
2024-02-23 21:09:10 -0500

    added files to repo

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //Code to import Budget.js
 import Budget from './components/Budget';
 
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseItem from './components/ExpenseItem';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
+
 // Add code to import the other components here under
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
 const App = () => {
     return (
         <AppProvider>
@@ -16,27 +22,27 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            <Budget/>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            <Remaining/>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            <ExpenseTotal/>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            <ExpenseList/>
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                            <ExpenseItem/>
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                            <AllocationForm/>
                         }        
 
                 </div>
